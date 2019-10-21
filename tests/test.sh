fcd65d649c0fc7146d130711879f28eaf9a21263
Barrett Lavonne <blavonne@at-e4.21-school.ru>
2019-10-21 11:18:08 +0300

    tests

--- tests/test.sh
+++ tests/test.sh
@@ -6,8 +6,29 @@
 #    By: blavonne <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/10/18 02:43:46 by blavonne          #+#    #+#              #
-#    Updated: 2019/10/18 02:46:03 by blavonne         ###   ########.fr        #
+#    Updated: 2019/10/18 23:30:14 by blavonne         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-../fillit 
+i=1
+
+for ((i = 1; i < 20; i++))
+	do
+		echo "Part 1. Test $i result:"
+		./fillit maps1/test$i
+		echo "____________\n"
+	done
+
+for ((i = 1; i < 17; i++))
+	do
+		echo "Part 2. Test $i result:"
+		./fillit maps2/test$i
+		echo "____________\n"
+	done
+
+for ((i = 1; i < 11; i++))
+	do
+		echo "Part 3. Test $i result:"
+		time ./fillit maps3/test$i
+		echo "____________\n"
+	done
